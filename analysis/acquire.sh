382cf2949f202ef2417e07eed9188053f3c558a3
abhijithanandan <abhijithananthan@gmail.com>
2017-08-15 17:57:49 +0530

    trial4

--- analysis/acquire.sh
+++
@@ -1,4 +0,0 @@
-awk  'NR>=24 && NR<=3024 {print $0}' thermo.log > thermo.dat 
-awk  'NR>=3072 && NR<=6072 {print $0}' thermo.log >> thermo.dat 
-awk  'NR>6114 && NR<=6614 {print $0}' thermo.log >> thermo.dat 
-awk  'NR>6663 && NR<=7663 {print $0}' thermo.log >> thermo.dat 
